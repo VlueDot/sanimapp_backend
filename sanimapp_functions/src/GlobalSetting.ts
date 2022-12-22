3a893c645473d89d77e41111b94d11286b21b158
Vincerr <rvin.rdgz@gmail.com>
2022-12-22 02:44:33 -0500

    working on the postman

--- sanimapp_functions/src/GlobalSetting.ts
+++ sanimapp_functions/src/GlobalSetting.ts
@@ -1,10 +1,17 @@
 
   export const odoo_url : string = 'https://oxe360-ooc-sanisol-staging-13-0-6571046.dev.odoo.com/web/session/authenticate';
     export const odoo_access = {
-        "jsonrpc": "2.0",
-        "method": "POST", 
-        "params":{
-            "db": "oxe360-ooc-sanisol-staging-13-0-6571046",
-            "login":"pablo@sanima.pe",
-            "password" : "Sanima2021"
-            }}
+      headers: { 'Content-Type': 'application/json' },
+      method: 'post',
+      body: JSON.stringify(
+        
+        {
+          "jsonrpc": "2.0",
+          "method": "call", 
+          "params":{
+              "db": "oxe360-ooc-sanisol-staging-13-0-6571046",
+              "login":"pablo@sanima.pe",
+              "password" : "Sanima2021"
+              }}
+            
+            )}
