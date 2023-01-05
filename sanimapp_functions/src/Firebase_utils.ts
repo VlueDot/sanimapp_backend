9cfdc85985007f6637d4362c25db049ba483c028
Vincerr <rvin.rdgz@gmail.com>
2023-01-05 14:51:45 -0500

    Todo Design syncs

--- sanimapp_functions/src/Firebase_utils.ts
+++ sanimapp_functions/src/Firebase_utils.ts
@@ -1,11 +1,31 @@
-export class Firebase_utils {
-    connectFirebase(){}
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin"
 
-    readFirebase(){
+const url_timestamps = "/timestamp_collection"
 
-    }
 
-    writeFirebase(){
+const serviceAccount = require('../service-account.json')
+const url_database = 'https://${serviceAccount.project_id}.firebaseio.com'
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: url_database
+})
 
-    }
-}
+
+// export class Firebase_utils {
+//     connectFirebase(){}
+
+//     readFirebase(){
+
+//     }
+
+//     writeFirebase(){
+
+//     }
+
+//     getTimestampFirebase(){
+
+//     }
+// }
+
+export const getTimestampsFirebase = functions.database.ref(url_database + url_timestamps).subscribe((data)=>{})
