25d19daeb94fff874c7184c9ea130343be9a04df
Vincerr <rvin.rdgz@gmail.com>
2023-01-05 13:13:02 -0500

    update

--- sanimapp_functions/src/index.ts
+++ sanimapp_functions/src/index.ts
@@ -2,20 +2,18 @@ import * as functions from "firebase-functions";
 import * as Odoo from "./Odoo_utils";
 // import { Firebase_utils } from "./Firebase_utils";
 
-export const TestFunction = functions.https.onRequest( (response) => {
-
+export const TestFunction = functions.https.onRequest( (request,response) => {
+  //do here whatever you must
+  response.send("TestFunction Finished")
 })
 
 
 export const OdooSync = functions.https.onRequest(async (request, response)=> {
+  //this will run with certain periodicity. This will be the stable function. 
   try{
     let odoo_login = await Odoo.OdooLogin();
-  
 
-
-
-
-
+    
 
     if (odoo_login == 1) Odoo.OdooLogout()
 
