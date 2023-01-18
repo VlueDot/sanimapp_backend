9d6a9ac6b0a619b82cf6617dd2d4338d35273794
Vincerr <rvin.rdgz@gmail.com>
2023-01-18 16:59:21 -0500

    advance

--- sanimapp_functions/src/Odoo_utils.ts
+++ sanimapp_functions/src/Odoo_utils.ts
@@ -99,9 +99,9 @@ export async function odooToFirebase_CRM_Campaigns(odoo_session:any) {
 //Have to delete that part
 export async function odooToFirebase_CRM_Tickets(odoo_session:any) {
 //First have to check the timestamp. we will leave it theere as backup in case something works as fuck
-const fb_timestamp = await FirebaseFcn.firebaseGet("/timestamp_collection/CMR_tickets_timestamp");
-const date = new Date(Number(fb_timestamp));
-const data_str = date.getFullYear()+"-"+("0" + (date.getMonth() + 1)).slice(-2)+"-"+("0" +date.getDate()).slice(-2)+" "+ ("0" +date.getHours()).slice(-2)+":"+("0" +date.getMinutes()).slice(-2)+":"+("0" +date.getSeconds()).slice(-2);
+// const fb_timestamp = await FirebaseFcn.firebaseGet("/timestamp_collection/CMR_tickets_timestamp");
+// const date = new Date(Number(fb_timestamp));
+// const data_str = "'"+ date.getFullYear()+"-"+("0" + (date.getMonth() + 1)).slice(-2)+"-"+("0" +date.getDate()).slice(-2)+" "+ ("0" +date.getHours()).slice(-2)+":"+("0" +date.getMinutes()).slice(-2)+":"+("0" +date.getSeconds()).slice(-2) + "'";
 
 const CustomHeaders: HeadersInit = {
   "Content-Type": "application/json",
@@ -111,11 +111,29 @@ const CustomHeaders: HeadersInit = {
 const raw = JSON.stringify({
   "params": {
     "model": "crm.lead",
+    "fields": [
+        "partner_id",
+        "campaign_id",
+        "stage_id",
+        "medium_id",
+        "source_id",
+        "referred",
+        "name",
+        "phone",
+        "mobile",
+        "tag_ids",
+        "create_uid",
+        "create_date"
+    ],
     "offset": 0,
-    "fields": ["partner_id","campaign_id","stage_id","medium_id",
-     "source_id","referred", "name","phone","mobile","tag_ids", "create_uid", "create_date", "write_date"]
-    },
-    "domain": ["write_date",">", data_str]
+    "domain": [
+        [
+            "write_date",
+            ">",
+            "2022-11-06 21:50:37"
+        ]
+    ]
+  }
 }
 
 );
@@ -130,10 +148,80 @@ console.log(params);
 
 const response = await fetch(settings.odoo_url + "dataset/search_read", params);
 const data = await response.json();
-
-
 console.log("data: " + JSON.stringify(data));
-console.log(data_str);
+const data_len = data["result"]["length"];
+const records = data["result"]["records"];
+
+
+
+for(var i = 0; i < data_len ; i++){
+  var stage_id = records[i]["stage_id"][0];
+  var partner_id="NaN"    //why Nan ? 
+  var campaign_id="NaN"
+  var medium_id="NaN"
+  var source_id="NaN"
+  var estateCRM="Cliente Nuevo"
+  // var typeService="Otro"
+
+
+  try {
+      campaign_id=records[i]["campaign_id"][1];
+      if (stage_id!=1) partner_id = records[i]["partner_id"][0];
+      medium_id=records[i]["medium_id"][1];
+      source_id=records[i]["source_id"][1];
+      let referred=records[i]["referred"];
+      let name=records[i]["name"];
+
+      let phone=records[i]["phone"];
+      if (phone=="false"){
+          phone="NaN"
+      }
+
+      let mobile=records[i]["mobile"];
+      if (mobile=="false"){
+          mobile="NaN"
+      }
+
+      let create_uid=records[i]["create_uid"][1];
+      let tag_ids=records[i]["tag_ids"];
+      let create_date=records[i]["create_date"];
+
+      switch (stage_id) {
+        case 1:
+          estateCRM = "Cliente Potencial" 
+          break;
+        case 2:
+          estateCRM = "Cliente con firma" 
+        case 3: 
+          estateCRM = "Cliente ganado"
+        case 4: 
+          estateCRM = "Cliente con Venta perdida"     
+        default:
+          estateCRM = "NaN"
+          break;
+      }
+    
+      var tag_ids_map = []
+      for (var i = 0; i < tag_ids.length; i++){
+        tag_ids_map.push (tag_ids[i])
+
+      }
+
+      console.log("tag_ids map" +tag_ids_map)
+      
+
+      console.log({partner_id, campaign_id,medium_id,source_id, referred, name, create_uid, tag_ids, create_date, estateCRM});
+  }
+  catch (error){
+    functions.logger.error( "[odooToFirebase_CRM_Tickets] ERROR: ", error);}
+  }
+
+  
+
+  console.log("stage_id", stage_id);
+
+ 
+
 
 
 
